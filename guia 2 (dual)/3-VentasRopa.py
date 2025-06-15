a5d4fad68ec68f8a2905d7db7e6f35224d695805
Gadhiell <gadhiell.chavez@alumnos.ulagos.cl>
2025-06-14 22:43:46 -0400

    primer avance

--- guia 2 (dual)/3-VentasRopa.py
+++ guia 2 (dual)/3-VentasRopa.py
@@ -1,1 +1,13 @@
-.
+"""Camilo y Karla"""
+
+# sueldo minimo en pesos chilenos
+SUELDOCLP = 529000
+
+# a) trabajadores y sus ventas diarias
+vendedores = {
+    "uno": [120000, 150000, 180000, 90000, 110000],
+     "dos": [200000, 220000, 250000, 180000, 300000],
+    "tres": [80000, 95000, 110000, 70000, 120000]
+}
+    
+print ("este es el reporte de ventas semanales")

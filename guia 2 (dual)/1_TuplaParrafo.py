3cdf9b738b0952cea486ee3aad29cbdf2573431b
Gadhiell <gadhiell.chavez@alumnos.ulagos.cl>
2025-06-14 22:41:54 -0400

    correcion

--- guia 2 (dual)/1_TuplaParrafo.py
+++ guia 2 (dual)/1_TuplaParrafo.py
@@ -2,12 +2,18 @@
 
 #Lee el texto ingresado
 texto = str(input('Ingrese el texto: \n'))
+if texto == "":
+    raise ValueError ("el texto no puede estar vacio")
+
+#Eliminar comas y puntos.
+texto_sc = texto.replace(',', '')
+texto_sp = texto_sc.replace('.', '')
 
 #Se pasa todo el texto a minúscula
-texto_min = texto.lower()
+texto_min = texto_sp.lower()
 
 #El texto ya en minúscula se guarda en una lista
-lista_texto = texto_min.split()
+lista_texto = lista_texto.split()
 
 #Ahora pide ingresar la palabra a contar
 palabra = str(input('Ahora ingrese una palabra para contar cuántas veces se repite en el texto: \n'))
@@ -15,7 +21,7 @@ palabra = str(input('Ahora ingrese una palabra para contar cuántas veces se rep
 #La palabra ingresada se guarda en minúscula todas sus letras
 palabra_min = palabra.lower()
 
-#Variable que cuenta cuántas veces se repitió la palabra ingresasa
+#Variable que cuenta cuántas veces se repitió la palabra ingresada
 repeticion = lista_texto.count(palabra_min)
 
 print(f'La palabra {palabra} se repite {repeticion} veces')
